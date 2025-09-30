58f3208037dbf0b861e29bca6d4156cb4cc34010
paco chinchay <pacochicnhay350@gmail.com>
2025-09-30 15:13:59 -0500

    feat: cine api

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,3 @@ Route::controller(PeliculaController::class)->prefix('peliculas')->group(functio
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
 });
-
-Route::controller(CineController::class)->group(function () {
-    Route::prefix('participacion')->group(function () {
-        Route::get('/{tipo}', 'getParticipacion');
-        Route::get('/{tipo}/{departamento}', 'getParticipacion');
-        Route::get('/{tipo}/{departamento}/{provincia}', 'getParticipacion');
-    });
-
-    Route::prefix('actas')->group(function () {
-        Route::get('/ubigeo/{pais}', 'getActasUbigeo');
-        Route::get('/ubigeo/{pais}/{departamento}', 'getActasUbigeo');
-        Route::get('/ubigeo/{pais}/{departamento}/{provincia}', 'getActasUbigeo');
-        Route::get('/ubigeo/{pais}/{departamento}/{provincia}/{distrito}', 'getActasUbigeo');
-        Route::get('/ubigeo/{pais}/{departamento}/{provincia}/{distrito}/{local}', 'getActasUbigeo');
-        Route::get('/ubigeo/{pais}/{departamento}/{provincia}/{distrito}/{local}/{grupo}', 'getActasUbigeo');
-
-        Route::get('/numero/{numero}', 'getActasNumero');
-    });
-});
